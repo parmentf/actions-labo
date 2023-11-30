1235228b3363898c3584124d9964e96327afd263
François Parmentier <francois.parmentier@gmail.com>
2023-11-30 16:36:41 +0100

    build(terms-teeft): Fix first parameter

--- bin/build-and-test.sh
+++ bin/build-and-test.sh
@@ -8,8 +8,10 @@ wait_for_url () {
     return 0
 }
 
-echo "Starting container"
-cd "services/$1"
+SERVICE_NAME=${1%%/*}
+
+echo "Starting container of $SERVICE_NAME"
+cd "services/$SERVICE_NAME"
 npm run start:dev
 
 echo "Waiting server to be ready"
@@ -18,7 +20,7 @@ wait_for_url "http://localhost:31976" 10
 echo "Ruuning hurl tests"
 hurl --variable host=http://localhost:31976 --test tests.hurl
 
-echo "Stopping container"
+echo "Stopping container of $SERVICE_NAME"
 npm run stop:dev
 
 echo "Done"
